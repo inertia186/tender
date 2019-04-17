11eeff9b65d5e3089e88a98f8b71f579e9b5d8dd
Anthony Martin <github@martin-studio.com>
2019-04-16 21:38:42 -0700

    sometimes the public nodes aren't accessible

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :public_steem_engine_blockchain, :steem_engine_blockchain
-  helper_method :head_block_num
+  helper_method :public_head_block_num, :head_block_num
   helper_method :replaying?
   
   def public_steem_engine_blockchain
@@ -11,11 +11,15 @@ class ApplicationController < ActionController::Base
     @steem_engine_blockchain ||= Radiator::SSC::Blockchain.new(root_url: ENV.fetch('STEEM_ENGINE_NODE_URL', 'https://api.steem-engine.com/rpc'))
   end
   
+  def public_head_block_num
+    public_steem_engine_blockchain.latest_block_info['blockNumber'] rescue nil || -1
+  end
+  
   def head_block_num
     @head_block_num ||= Transaction.maximum(:block_num) || -1
   end
   
   def replaying?
-    (public_steem_engine_blockchain.latest_block_info['blockNumber'] - head_block_num).abs > 48
+    (public_head_block_num - head_block_num).abs > 48
   end
 end
