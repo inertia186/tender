c68675dfff7f561372fba321157d667e103bfc85
Anthony Martin <github@martin-studio.com>
2019-05-13 09:58:53 -0700

    block zero now defines ENG token, thus we no longer need to hardcode its existence in the UI

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -7,20 +7,6 @@ class TokensController < ApplicationController
     @tokens = TokensCreate.joins(:trx).includes(:trx)
     @tokens = @tokens.order(Transaction.arel_table[:timestamp].asc)
     @tokens = @tokens.paginate(per_page: @per_page, page: @page)
-    
-    @eng_token = if Transaction.any? && @page == 1
-      TokensCreate.new(
-        id: 0,
-        trx: Transaction.find_by(trx_id: '0', trx_in_block: 0),
-        symbol: 'ENG',
-        name: 'Steem Engine Token',
-        url: 'https://steem-engine.com',
-        precision: 8,
-        max_supply: 9007199254740991,
-        updated_at: Time.at(1514793600),
-        created_at: Time.at(1514793600)
-      )
-    end
   end
   
   def show
