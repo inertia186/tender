df30905ca20de361b5934487fd85321421a3adc7
Anthony Martin <github@martin-studio.com>
2019-06-20 11:17:46 -0700

    added token precision helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def token_precision(token)
+    TokensUpdatePrecision.order(timestamp: :desc).where(symbol: token.symbol).first.try(:precision) || token.precision
+  end
 end
