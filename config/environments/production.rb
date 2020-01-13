13a78acf3d385766d9950b9bdce6104f11dbaaa8
Anthony Martin <github@martin-studio.com>
2020-01-13 13:01:04 -0800

    production logging defaults

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.logger = ActiveSupport::Logger.new(config.paths['log'].first, 1, 50 * 1024 * 1024)
+  
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
