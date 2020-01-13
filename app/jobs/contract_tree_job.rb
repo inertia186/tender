8f9046f0cbb9c3bab5b74fd0e86596e330336b0a
Anthony Martin <github@martin-studio.com>
2020-01-13 15:13:14 -0800

    extracted contract tree render to external job

---
+++ app/jobs/contract_tree_job.rb
@@ -0,0 +1,29 @@
+class ContractTreeJob < ActiveJob::Base
+  include ActionView::Helpers::UrlHelper
+  include Rails.application.routes.url_helpers
+  
+  cattr_reader :cached_partial, :render_at
+  attr_reader :controller
+  
+  def self.perform
+    @@cached_partial = nil if !!@@render_at && @@render_at < 1.hour.ago
+    
+    @@cached_partial || ContractTreeJob.new.send(:render)
+  end
+private
+  def render
+    @@cached_partial = ''
+    
+    Thread.new do
+      latest_transactions = Transaction.where('timestamp > ?', 24.hours.ago).where.not(contract: %w(null token))
+      latest_transactions.order(:contract).distinct.pluck(:contract).each do |contract|
+        template = File.read("#{Rails.root}/app/views/dashboard/_contract_tree.html.haml")
+        renderer = Haml::Engine.new(template)
+        @@cached_partial = renderer.render(binding, locals: {latest_transactions: latest_transactions, contract: contract})
+        @@render_at = Time.now
+      end
+    end
+    
+    @@cached_partial
+  end
+end
