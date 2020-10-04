2847119d4b0880fbb146f6405ff34df7beb7bda5
Anthony Martin <github@martin-studio.com>
2020-10-04 00:04:01 -0700

    health controller fix

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -3,22 +3,21 @@ class HealthController < ApplicationController
   
   def index
     redis_ctx = Redis.new(url: ENV.fetch('MEESEEKER_REDIS_URL', 'redis://127.0.0.1:6379/0'))
-    steem_block_num = redis_ctx.get("#{chain}:meeseeker:last_block_num").to_i
+    block_num = redis_ctx.get("#{engine_chain_key_prefix}:meeseeker:last_block_num").to_i
     engine_block_num = redis_ctx.get("#{engine_chain_key_prefix}:meeseeker:last_block_num").to_i
-    steem_time = nil
-    @steem_head_block_num = steem_head_block_num
-    @datetime = steem_datetime
+    @head_block_num = head_block_num
+    @datetime = Time.now
     @engine_head_block_num = nil
     
     @engine_head_block_num = head_block_num
     
-    steem_block_num_diff = @steem_head_block_num - steem_block_num
+    block_num_diff = @head_block_num - block_num
     engine_block_num_diff = @engine_head_block_num - engine_block_num
     external_engine_block_num_diff = @engine_head_block_num - public_head_block_num
     
     @status = 'OK'
     @messages = []
-    @messages << "Last steem engine block_num indexed: #{engine_block_num}; head: #{@engine_head_block_num}; diff: #{engine_block_num_diff}"
+    @messages << "Last engine block_num indexed: #{engine_block_num}; head: #{@engine_head_block_num}; diff: #{engine_block_num_diff}"
     
     if engine_block_num_diff.abs > 48 || external_engine_block_num_diff.abs > 48
       @status = 'ERROR'
